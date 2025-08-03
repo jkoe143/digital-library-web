a5130176941636ff9c4d9d0b5425a1fae8057f7e
jkoe143 <jkoe143@gmail.com>
2025-08-03 18:42:56 -0500

    Update API URL to point to deployed backend

--- src/api/BookService.js
+++ src/api/BookService.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/books";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8080";
 
 export const getBooks = async (page = 0, size = 10) => {
-  return await axios.get(`${API_URL}?page=${page}&size=${size}`);
+  return await axios.get(`${API_URL}/books?page=${page}&size=${size}`);
 };
 
 export const getBook = async (id) => {
-  return await axios.get(`${API_URL}/${id}`);
+  return await axios.get(`${API_URL}/books/${id}`);
 };
 
 export const createBook = async (book) => {
-  return await axios.post(API_URL, book);
+  return await axios.post(`${API_URL}/books`, book);
 };
 
 export const updateBook = async (book) => {
-  return await axios.put(`${API_URL}/${book.id}`, book);
+  return await axios.put(`${API_URL}/books/${book.id}`, book);
 };
 
 export const updatePhoto = async (formData) => {
-  return await axios.post(`${API_URL}/photo`, formData, {
+  return await axios.post(`${API_URL}/books/photo`, formData, {
     headers: {
       "Content-Type": "multipart/form-data",
     },
@@ -27,5 +27,5 @@ export const updatePhoto = async (formData) => {
 };
 
 export const deleteBook = async (id) => {
-  return await axios.delete(`${API_URL}/${id}`);
+  return await axios.delete(`${API_URL}/books/${id}`);
 };
